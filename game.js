31b607e1c8303a36b57297f0d10da952b585807d
nguyenle <nle20959@gmail.com>
2020-07-12 01:00:45 +0700

    Add logic

--- game.js
+++ game.js
@@ -12,12 +12,16 @@ We also load all of our images.
 
 let canvas;
 let ctx;
-
 canvas = document.createElement("canvas");
+canvas.className = "canvasGame maxSize";
+//canvas.position = "absolute"
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 ctx = canvas.getContext("2d");
-canvas.width = 700;
-canvas.height = 500;
 document.body.appendChild(canvas);
+// canvas.width = 1024;
+// canvas.height = 1024;
+
 console.log(screen.width)
 console.log(screen.height)
 
@@ -30,15 +34,22 @@ let elapsedTime = 0;
 let score = 0;
 let userName = document.getElementById("userName").innerHTML;
 const NUMBER_OBJECTS = 5;
-const SIZE = 60;
-const V1 = 10;
-const V2 = 15;
-let currentNumber = 0;
+const NUMBER_MICE = 3;
+const LIVES = 3;
+let SIZE = 60;
+const MON_HEIGHT = 60;
+const MON_WIDTH = 55;
+const RATIO = 20;
+const V1 = 70;
+const V2 = 150;
+let currentBran = [];
+let currentMice = [];
+let lives = LIVES;
 
 function getTime() {
   var d = new Date();
   var n = d.getTime();
-  return n;
+  return n/1000;
 
 }
 
@@ -169,75 +180,105 @@ function getReady() {
     }
 
 
-    if (monX > canvas.width - 32) {
+    if (monX > canvas.width - MON_WIDTH/2) {
       monX = 0;
     }
 
     if (monX < 0) {
-      monX = canvas.width - 32;
+      monX = canvas.width - MON_WIDTH/2;
     }
     //
 
-    monY = canvas.height - 62;
+    monY = canvas.height - MON_HEIGHT;
 
 
-    if (cakeX > canvas.width - 32) {
-      cakeX = 0;
+
+    function randomX(){
+      return SIZE/2 + Math.random()*(canvas.width-SIZE);
     }
 
-    if (cakeX < 0) {
-      cakeX = canvas.width - 32;
-    }
-    //
-
-    if (cakeY < 0) {
-      cakeY = canvas.height - 32;
-    }
-
-    if (cakeY > canvas.height - 32) {
-      cakeY = 0;
+    function getV(){
+      return V1+(V2-V1)*Math.random();
     }
 
 
     // Create and Update cakes
-      if(currentNumber < NUMBER_OBJECTS){
-          let cakeImage = new Image();
-          cakeImage.src = "images/bran.png"
-          cakeImage.y = canvas.height/2;
-          function random(){
-            return Math.random()*canvas.width;
+      if(currentBran.length < NUMBER_OBJECTS){
+          let cakeImg = new Image();
+          cakeImg.src = "images/bran.png"
+          cakeImg.toadoY = -SIZE/2;
+          cakeImg.toadoX = randomX();
+          cakeImg.veloc = getV();
+          cakeImg.t1 = getTime();
+          cakeImg.onload = function(){
+            cakeImg.loaded = true;
           }
-          cakeImage.x = random();
-
-          function getV(){
-            return V1+(V2-V1)*Math.random();
-          }
-
-          cakeImage.v = getV();
-          cakeImage.t0 = getTime();
-          // cakeImage.onload = function(){
-            ctx.drawImage(cakeImage, cakeImage.x, cakeImage.y);
-          //}
-          currentNumber++;
+          currentBran.push(cakeImg)
       }
+      currentBran.forEach((bran)=> {
+        if (!((monX -bran.toadoX) > SIZE || (bran.toadoX - monX) > SIZE)
+         && !((monY - bran.toadoY) > SIZE)
+        ){
+          score++;
+          currentBran.splice(currentBran.indexOf(bran), 1);
+        } 
+        else if (bran.toadoY > canvas.height){
+          currentBran.splice(currentBran.indexOf(bran), 1);
+        }
+        else{
+          let t2 = getTime()
+          bran.toadoY = -SIZE/2 + bran.veloc * (t2 - bran.t1)
+        }
+      })
+
+      
+    // Create and Update Mice
+    if(currentMice.length < NUMBER_MICE){
+      let mouseImg = new Image();
+      mouseImg.src = "images/mouse.png"
+      mouseImg.toadoY = -SIZE/2;
+      mouseImg.toadoX = randomX();
+
+      mouseImg.veloc = getV();
+      mouseImg.t1 = getTime();
+      mouseImg.onload = function(){
+        mouseImg.loaded = true;
+      }
+      currentMice.push(mouseImg)
+  }
+  currentMice.forEach((mouse)=> {
+    if (!((monX - mouse.toadoX) > SIZE || (mouse.toadoX - monX) > SIZE)
+     && !((monY - mouse.toadoY) > SIZE)
+    ){
+      lives--;
+      currentMice.splice(currentMice.indexOf(mouse), 1);
+    }
+     else if (mouse.toadoY > canvas.height){
+      currentMice.splice(currentMice.indexOf(mouse), 1);
+    }
+    else{
+      let t2 = getTime()
+      mouse.toadoY = -SIZE/2 + mouse.veloc * (t2 - mouse.t1)
+    }
+  })
 
 
 
 
     // Check if player and monster collided. Our images
     // are about 32 pixels big.
-    if (
-      monX <= (cakeX + 32)
-      && cakeX <= (monX + 32)
-      && monY <= (cakeY + 32)
-      && cakeY <= (monY + 32)
-    ) {
-      // Pick a new location for the monster.
-      // Note: Change this to place the monster at a new, random location.
-      cakeX = Math.random() * canvas.width;
-      cakeY = Math.random() * canvas.width;
-      score++;
-    }
+    // if (
+    //   monX <= (cakeX + 32)
+    //   && cakeX <= (monX + 32)
+    //   && monY <= (cakeY + 32)
+    //   && cakeY <= (monY + 32)
+    // ) {
+    //   // Pick a new location for the monster.
+    //   // Note: Change this to place the monster at a new, random location.
+    //   cakeX = Math.random() * canvas.width;
+    //   cakeY = Math.random() * canvas.width;
+    //   score++;
+    // }
 
   };
 
@@ -249,14 +290,23 @@ function getReady() {
    */
   var render = function () {
     if (bgReady) {
-      ctx.drawImage(bgImage, 0, 0);
+      ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
     }
     if (heroReady) {
-      ctx.drawImage(monImage, monX, monY);
+      ctx.drawImage(monImage, monX, monY, MON_WIDTH, MON_HEIGHT);
     }
-    if (monsterReady) {
-      ctx.drawImage(cakeImage, cakeX, cakeY);
-    }
+
+    currentBran.forEach((bran) => {
+      if(bran.loaded){
+        ctx.drawImage(bran, bran.toadoX, bran.toadoY);
+      }
+    })
+
+    currentMice.forEach((mouse) => {
+      if(mouse.loaded){
+        ctx.drawImage(mouse, mouse.toadoX, mouse.toadoY);
+      }
+    })
     if ((SECONDS_PER_ROUND - elapsedTime) == 0) {
 
       ctx.fillText(`Game Over`, 20, 130);
